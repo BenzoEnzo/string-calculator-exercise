11066992bf6a4a20928db93d7ed454f85615fa29
DELL <goxer@o2.pl>
2025-07-10 21:34:57 +0200

    added own method for splitting text into parts

--- calculator/src/main/java/pl/bartkub/exercise/calculator/Parser.java
+++ calculator/src/main/java/pl/bartkub/exercise/calculator/Parser.java
@@ -1,5 +1,8 @@
 package pl.bartkub.exercise.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
@@ -13,5 +16,34 @@ public class Parser {
         }
 
         return "[,\n]";
+    }
+
+    public List<String> divideInput(String input) {
+        if (input.startsWith("//")) {
+            input = input.substring(input.indexOf('\n') + 1);
+        }
+
+        List<String> parts = new ArrayList<>();
+
+        Matcher matcher = Pattern.compile("-?\\d+").matcher(input);
+
+        int index = 0;
+
+        while (matcher.find()) {
+            int start = matcher.start();
+
+            if (index < start)
+                parts.add(input.substring(index, start));
+
+            parts.add(matcher.group());
+
+            index = matcher.end();
+        }
+
+        if (index < input.length()) {
+            parts.add(input.substring(index));
+        }
+
+        return parts;
     }
 }
--- calculator/src/test/java/pl/bartkub/exercise/calculator/ParserTest.java
+++ calculator/src/test/java/pl/bartkub/exercise/calculator/ParserTest.java
@@ -2,6 +2,8 @@ package pl.bartkub.exercise.calculator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
@@ -20,5 +22,35 @@ public class ParserTest {
         String delimiter = parser.extractDelimiter("//sepq\n1sepq2sepq3sepq4");
 
         assertEquals("\\Qsepq\\E", delimiter);
+    }
+
+    @Test
+    void shouldCorrectlyDivideInputWithNegativeNumber() {
+        String input = "1,-44";
+        List<String> expected = List.of("1", ",", "-44");
+
+        List<String> result = parser.divideInput(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void shouldCorrectlyDivideInputWithBaseSeparator() {
+        String input = "1,4 5\n";
+        List<String> expected = List.of("1", ",", "4", " ", "5", "\n");
+
+        List<String> result = parser.divideInput(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void shouldCorrectlyDivideInputWithCustomSeparator() {
+        String input = "//sepq\n1sepq5,4;";
+        List<String> expected = List.of("1", "sepq", "5", ",", "4", ";");
+
+        List<String> result = parser.divideInput(input);
+
+        assertEquals(expected, result);
     }
 }
